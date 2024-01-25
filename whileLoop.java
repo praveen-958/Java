85f6c056a81304eecffd1594e6f4cb590f78b4b9
praveen <praveenchouhan7272@gmail.com>
2024-01-25 22:38:46 +0530

    second commit

---
+++ whileLoop.java
@@ -0,0 +1,16 @@
+package LOOPS;
+
+/**
+ * whileLoop
+ */
+public class whileLoop {
+
+  public static void main(String[] args) {
+
+    int i = 0;
+    while(i <= 100){
+        System.out.println(i);
+        i++;
+    }
+  }
+}
